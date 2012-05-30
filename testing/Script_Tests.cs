e317fdf6c4e1d05ae0c92fe52cb5e5345501e885
Martin <Nitramtj@users.noreply.github.com>
2012-05-30 13:21:24 -0500

    Finished simple test that loads, saves and then loads a file again.

--- testing/Script_Tests.cs
+++ testing/Script_Tests.cs
@@ -4,5 +4,18 @@
 
 function VBL_SaveLoadTest(%file)
 {
+	%vbl = newVBL();
+	%vbl.loadBLSFile(%file);
+	%vbl.createBricks();
 	
+	%vbl.shiftBricks(%vbl.getSizeX() SPC "0 0");
+	%vbl.exportBLSFile("config/ToolBrick/tests/saveload.bls");
+	%vbl2 = newVBL();
+	%vbl2.loadBLSFile(%file);
+	
+	%vbl.createBricks();
+	%vbl2.createBricks();
+	
+	%vbl.delete();
+	%vbl2.delete();
 }
